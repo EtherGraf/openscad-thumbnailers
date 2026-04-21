19c7eecc59d0792f2f3cc8055cff881fd1f6b64c
dfredell <furtchet@gmail.com>
2026-04-20 20:49:13 -0500

    Fix issue with spaces in source stl filenames

--- stl2png.sh
+++ stl2png.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-SCAD=/tmp/stl2png_$(basename $2).scad
-echo "import(\"$1\");" > $SCAD
-/usr/bin/openscad  --colorscheme="Cornfield" --imgsize=$3,$3 -o $2.png $SCAD
+SCAD="/tmp/stl2png_$(basename $2).scad"
+echo "import(\"$1\");" > "$SCAD"
+/usr/bin/openscad  --colorscheme="Cornfield" --imgsize=$3,$3 -o "$2.png" $SCAD
 rm $SCAD
-/usr/bin/convert $2.png -fuzz 20% -transparent white png:$2
-rm $2.png
+/usr/bin/convert "$2.png" -fuzz 20% -transparent white "png:$2"
+rm "$2.png"
