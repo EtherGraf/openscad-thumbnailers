9c16d144fccad0c8e888899b7ccbfd2b560f206e
dfredell <furtchet@gmail.com>
2026-04-20 21:01:57 -0500

    fix basename call

--- stl2png.sh
+++ stl2png.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
-SCAD="/tmp/stl2png_$(basename $2).scad"
+BASENAME=$(basename "$2")
+SCAD="/tmp/stl2png_$BASENAME.scad"
 echo "import(\"$1\");" > "$SCAD"
-/usr/bin/openscad  --colorscheme="Cornfield" --imgsize=$3,$3 -o "$2.png" $SCAD
-rm $SCAD
+/usr/bin/openscad  --colorscheme="Cornfield" --imgsize=$3,$3 -o "$2.png" "$SCAD"
+rm "$SCAD"
 /usr/bin/convert "$2.png" -fuzz 20% -transparent white "png:$2"
 rm "$2.png"
